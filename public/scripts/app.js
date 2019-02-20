a6a498803bb82ceab84665cb94928b6b97c385f2
John <connollyjohnf@gmail.com>
2019-02-20 01:18:03 +0000

    added Tweeter feed container, filled with placeholder text. Events for hovering on and off change opacity of said container. No icons (like, flag, etc) added yet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const tweetbox = $(".tweet-container");
+  console.log(tweetbox);
+  tweetbox.on("mouseover", function(event) {
+    $(".tweet-container").css("opacity", "1");
+  });
+  tweetbox.on("mouseout", function(event) {
+    $(".tweet-container").css("opacity", ".75");
+  });
+});
